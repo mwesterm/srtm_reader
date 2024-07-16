0487fe3266f756bcf5dbd0d5214254540de18c50
Jeromos Kovacs <iitsnotme214@proton.me>
2024-07-16 23:48:09 +0200

    fix: created `struct Coord`, which makes things easier to use and less error-prone

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use std::io;
 use std::io::{BufReader, Read};
 use std::path::Path;
 
-const EXTENT: u32 = 3600;
+const EXTENT: usize = 3600;
 
 #[derive(PartialEq, Eq, Clone, Copy, Debug)]
 pub enum Resolution {
@@ -16,14 +16,14 @@ pub enum Resolution {
 }
 
 impl Resolution {
-    pub const fn extent(&self) -> u32 {
+    pub const fn extent(&self) -> usize {
         match self {
             Resolution::SRTM05 => EXTENT * 2 + 1,
             Resolution::SRTM1 => EXTENT + 1,
             Resolution::SRTM3 => EXTENT / 3 + 1,
         }
     }
-    pub const fn total_size(&self) -> u32 {
+    pub const fn total_size(&self) -> usize {
         self.extent().pow(2)
     }
 }
@@ -43,6 +43,22 @@ pub enum Error {
     Read,
 }
 
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Coord {
+    lat: f64,
+    lon: f64,
+}
+impl Coord {
+    pub fn trunc(&self) -> (i32, i32) {
+        (self.lat.trunc() as i32, self.lon.trunc() as i32)
+    }
+}
+impl<F1: Into<f64>, F2: Into<f64>> From<(F1, F2)> for Coord {
+    fn from(value: (F1, F2)) -> Self {
+        let (lat, lon) = (value.0.into(), value.1.into());
+        Coord { lat, lon }
+    }
+}
 impl Tile {
     fn new_empty(lat: i32, lng: i32, res: Resolution) -> Tile {
         Tile {
@@ -71,32 +87,44 @@ impl Tile {
     pub fn max_height(&self) -> i16 {
         *(self.data.iter().max().unwrap())
     }
-    fn get_origin(&self, coord: (f64, f64)) -> (f64, f64) {
-        let lat = coord.0.trunc() + 1.; // The latitude of the lower-left corner of the tile
-        let lon = coord.1.trunc(); // The longitude of the lower-left corner of the tile
-        (lat, lon)
+    fn get_origin(&self, coord: Coord) -> Coord {
+        let lat = coord.lat.trunc() + 1.; // The latitude of the lower-left corner of the tile
+        let lon = coord.lon.trunc(); // The longitude of the lower-left corner of the tile
+        Coord { lat, lon }
     }
-    fn get_offset(&self, coord: (f64, f64)) -> (usize, usize) {
+    fn get_offset(&self, coord: Coord) -> (usize, usize) {
         let origin = self.get_origin(coord);
         // eprintln!("origin: ({}, {})", origin.0, origin.1);
         let extent = self.resolution.extent() as f64;
 
-        let row = ((origin.0 - coord.0) * extent) as usize;
-        let col = ((coord.1 - origin.1) * extent) as usize;
+        let row = ((origin.lat - coord.lat) * extent) as usize;
+        let col = ((coord.lon - origin.lon) * extent) as usize;
         (row, col)
     }
 
-    pub fn get(&self, coord: (f64, f64)) -> i16 {
+    pub fn get(&self, coord: Coord) -> i16 {
         let offset = self.get_offset(coord);
+        let lat = coord.lat.trunc() as i32;
+        let lon = coord.lon.trunc() as i32;
+        assert!(
+            self.latitude <= lat,
+            "hgt lat: {}, coord lat: {lat}",
+            self.latitude
+        );
+        assert!(
+            self.longitude <= lon,
+            "hgt lon: {}, coord lon: {lon}",
+            self.longitude
+        );
         // eprintln!("offset: ({}, {})", offset.1, offset.0);
-        self.get_at_offset(offset.1 as u32, offset.0 as u32)
+        self.get_at_offset(offset.1, offset.0)
     }
 
-    fn get_at_offset(&self, x: u32, y: u32) -> i16 {
+    fn get_at_offset(&self, x: usize, y: usize) -> i16 {
         self.data[self.idx(x, y)]
     }
 
-    fn idx(&self, x: u32, y: u32) -> usize {
+    fn idx(&self, x: usize, y: usize) -> usize {
         assert!(x < self.resolution.extent() && y < self.resolution.extent());
         (y * self.resolution.extent() + x) as usize
     }
@@ -117,7 +145,10 @@ fn get_resolution<P: AsRef<Path>>(path: P) -> Option<Resolution> {
             None
         }
     };
-    fs::metadata(path).ok().and_then(from_metadata)
+    fs::metadata(path)
+        .inspect_err(|e| eprintln!("error: {e:#?}"))
+        .ok()
+        .and_then(from_metadata)
 }
 
 // FIXME Better error handling.
@@ -136,11 +167,11 @@ fn get_lat_long<P: AsRef<Path>>(path: P) -> Result<(i32, i32), Error> {
     let lng: i32 = desc[4..7].parse().map_err(|_| Error::ParseLatLong)?;
     Ok((lat_sign * lat, lng_sign * lng))
 }
-pub fn get_filename(coord: (f64, f64)) -> String {
-    let lat_ch = if coord.0 >= 0. { 'N' } else { 'S' };
-    let lon_ch = if coord.1 >= 0. { 'E' } else { 'W' };
-    let lat = coord.0.trunc() as u16;
-    let lon = coord.1.trunc() as u16;
+pub fn get_filename(coord: Coord) -> String {
+    let lat_ch = if coord.lat >= 0. { 'N' } else { 'S' };
+    let lon_ch = if coord.lon >= 0. { 'E' } else { 'W' };
+    let lat = coord.lat.trunc() as i32;
+    let lon = coord.lon.trunc() as i32;
     format!(
         "{lat_ch}{lat}{lon_ch}{}{lon}.hgt",
         if lon < 100 { "0" } else { "" }
