7273959d619899c1395b36a0f43ad537fc4f88c3
Jeromos Kovács <iitsnotme214@proton.me>
2025-01-25 17:42:11 +0100

    refactor(tests): merge several fn-s

--- src/tests.rs
+++ src/tests.rs
@@ -3,10 +3,10 @@ use std::path::Path;
 
 #[test]
 fn parse_latitute_and_longitude() {
-    let ne = Path::new("N35E138.hgt");
+    let ne = Path::new("/tmp/N35E138.hgt");
     assert_eq!(Tile::get_lat_lon(ne).unwrap(), (35, 138));
 
-    let nw = Path::new("N35W138.hgt");
+    let nw = Path::new("/home/juhuu/DTM of me self/N35W138.hgt");
     assert_eq!(Tile::get_lat_lon(nw).unwrap(), (35, -138));
 
     let se = Path::new("S35E138.hgt");
@@ -29,62 +29,36 @@ fn extents() {
 }
 
 #[test]
-#[should_panic]
-fn wrong_coord_0() {
-    let _ = Coord::new(-190, 42.4);
+fn wrong_coords() {
+    let coord_new_panics = |lat: f64, lon: f64| assert!(Coord::opt_new(lat, lon).is_none());
+    coord_new_panics(-190., 42.4);
+    coord_new_panics(180., -42.4);
+    coord_new_panics(-90., 181.);
+    coord_new_panics(90., -180.00001);
 }
 #[test]
-#[should_panic]
-fn wrong_coord_1() {
-    let _ = Coord::new(180, -42.4);
-}
-#[test]
-#[should_panic]
-fn wrong_coord_2() {
-    let _ = Coord::new(-90., 181.);
-}
-#[test]
-#[should_panic]
-fn wrong_coord_3() {
-    let _ = Coord::new(90., -180.00001);
-}
-#[test]
-fn correct_coord_0() {
-    let _ = Coord::new(-90, 180);
-}
-#[test]
-fn correct_coord_1() {
-    let _ = Coord::new(90, -180);
-}
-#[test]
-fn correct_coord_2() {
+fn correct_coords() {
+    let coord_new = |lat: f64, lon: f64| assert!(Coord::opt_new(lat, lon).is_some());
+    coord_new(-90., 180.);
+    coord_new(90., -180.);
+
     let c = Coord::new(90, -180).with_lon(-85.7);
     assert_eq!(Coord::new(90, -85.7), c);
-}
-#[test]
-fn correct_coord_3() {
+
     let c = Coord::new(90, -180).with_lat(0.3);
     assert_eq!(Coord::new(0.3, -180), c);
-}
-#[test]
-fn correct_coord_4() {
+
     let c = Coord::new(90, -180).with_lat(0.3).with_lon(83.3);
     assert_eq!(Coord::new(0.3, 83.3), c);
-}
-#[test]
-fn correct_coord_5() {
+
     let c: Coord = (90, -180).into();
     let c = c.with_lat(0.3).with_lon(83.3);
     assert_eq!(Coord::new(0.3, 83.3), c);
-}
-#[test]
-fn correct_coord_6() {
+
     let c: Coord = (90, -180).into();
     let c = c.with_lat(0.3).with_lon(83.3);
     assert_eq!(Coord::new(0.3, 83.3), c);
-}
-#[test]
-fn correct_coord_7() {
+
     let c: Coord = (-90, 180).into();
     let c = c.add_to_lat(0.3252).add_to_lon(-3.2);
     assert_eq!(Coord::new(-89.6748, 176.8), c);
@@ -98,14 +72,12 @@ fn file_names() {
         .iter()
         .map(|c| Coord::from(*c).get_filename())
         .collect::<Vec<_>>();
-    assert_eq!(fnames[0], "N45E001.hgt");
-    assert_eq!(fnames[1], "S02E087.hgt");
-    assert_eq!(fnames[2], "N35W007.hgt");
+    assert_eq!(fnames, ["N45E001.hgt", "S02E087.hgt", "N35W007.hgt"]);
 }
 #[test]
 fn read() {
     let coord = Coord::new(44.4480403, 15.0733053);
-    let fname = Coord::from(coord).get_filename();
+    let fname = coord.get_filename();
     let tile = Tile::from_file(fname).unwrap();
     assert_eq!(tile.latitude, 44);
     assert_eq!(tile.longitude, 15);
